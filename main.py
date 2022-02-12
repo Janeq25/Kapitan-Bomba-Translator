55636643744885b5391ee4e2c482775c601ab2c2
Janek Cichoń <janek2510@hotmail.com>
2022-02-12 21:06:22 +0100

    sprawna tablica 3d

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.metrics import dp
 from kivy.properties import NumericProperty
+from kivy.uix.textinput import TextInput
+
 
 import json
 from difflib import get_close_matches
@@ -24,6 +26,8 @@ numerek to będzie ile razy dane tłumaczenie zostało wybrane
 bo zrobie jeszcze taki paseczek gdzie program będzie podawał
 ze trzy najlepsze wyniki i se będzie można wybrać
 '''
+
+
 
 
 dictionary = [[]] # [['polskie', 'kosmiczne', 'cyferka'], [p, k, c], [p, k, c]]
@@ -46,15 +50,51 @@ def load_dictionary():
         values.append(entry[1])
 
 
-    print(dictionary)
-    print('================')
-    print(keys)
-    print('================')
-    print(values)
+    # print(dictionary)
+    # print('================')
+    # print(keys)
+    # print('================')
+    # print(values)
+
+
+def find_matching_keys(maching_values):
+    global keys, values
+    '''
+    funkcja dostaje liste słówek po niepolsku
+    zwraca poukładaną listę słówek po polsku
+    '''
+    maching_keys = []
+    for i, value in enumerate(values):
+        for j, maching_value in enumerate(maching_values):
+            if value == maching_value:
+                maching_keys.append(keys[i])
+                del maching_values[j]
+            if len(maching_values) <= 0:
+                return maching_keys
+    return maching_keys
+
+def find_matching_values(maching_keys):
+    global keys, values
+    '''
+    funkcja dostaje liste słówek po polsku
+    zwraca poukładaną listę słówek po niepolsku
+    '''
+    maching_values = []
+    for i, key in enumerate(keys):
+        for j, maching_key in enumerate(maching_keys):
+            if key == maching_key:
+                maching_values.append(values[i])
+                del maching_keys[j]
+            if len(maching_keys) <= 0:
+                return maching_values
+    return maching_values
+
 
 
 
 load_dictionary()
+
+
 
 class MainScreen(Screen):
     translation_mode = NumericProperty(0)
@@ -65,23 +105,57 @@ class MainScreen(Screen):
     def switch_mode(self):
         if self.translation_mode == 0:
             self.translation_mode = 1
+            
         elif self.translation_mode == 1:
             self.translation_mode = 0
 
-    def update(self, text):
+    def translate(self, *args):
+        #he he
+
+        '''
+        Co sie tu odpierdala?
+        świetne pytanie
+        nie wiem
+        ale tak sprawdzamy status guziczka do zmiany kierunku tłumaczenia
+        na 0 tłumaczymy z polskiego na nasze
+        na 1 z naszego na polskie
+
+        tłumaczenie polega na podzieleniu wpisanego tekstu
+        niestety według spacji czyli związki takie jak cyborg -> umo del mahineri
+        działają tylko w jedną stronę
+        Moze kiedyś wymyślę jak to naprawić
+
+        potem po podzieleniu szukamy najbliżej wyglądających słów w listach keys (wyrazy po polsku) lub values (wyrazy po niepolsku)
+        potem wiedząc, że listy keys i values są w takiej samej kolejności, czyli key[0] to polskie tłumaczenie value[0],
+        tworzymy listę znalezionych tłumaczeń
+        pierwszy element tej listy zostaje wpisany do okienka z tłumaczeniem
+        '''
 
         if self.translation_mode == 0:
+            text = self.ids['input'].text
             words = text.split(' ')
             out = ''
+            #print(words)
             for word in words:
                 matching_keys = get_close_matches(word, keys) #list of potential key candidates
-                matching_values = []
-                for entry in dictionary:
-                    for key in matching_keys:
-                        if key == entry[0]:
-                            matching_values.append(entry[1])
+                matching_values = find_matching_values(matching_keys)
 
-                if matching_keys:
+                #print(matching_values)
+
+                if matching_values:
+                    if len(matching_values) > 1:
+                        self.ids['input1'].text = matching_values[1]
+                    else:
+                        self.ids['input1'].text = ''
+                    if len(matching_values) > 2:
+                        self.ids['input2'].text = matching_values[2]
+                    else:
+                        self.ids['input2'].text = ''
+                    if len(matching_values) > 3:
+                        self.ids['input3'].text = matching_values[3]
+                    else:
+                        self.ids['input3'].text = ''
+
                     out += matching_values[0] + ' '
                 else:
                     out += word + ' '
@@ -89,22 +163,35 @@ class MainScreen(Screen):
             self.ids.output.text = out
 
         elif self.translation_mode == 1:
+            text = self.ids['output'].text
             words = text.split(' ')
             out = ''
+            #print(words)
             for word in words:
                 matching_values = get_close_matches(word, values) #list of potential key candidates
-                matching_keys = []
-                for entry in dictionary:
-                    for value in matching_values:
-                        if value == entry[1]:
-                            matching_keys.append(entry[2])
+                matching_keys = find_matching_keys(matching_values)
+ 
+                #print(matching_keys)
 
-                if matching_values:
+                if matching_keys:
+                    if len(matching_keys) > 1:
+                        self.ids['output1'].text = matching_keys[1]
+                    else:
+                        self.ids['output1'].text = ''
+                    if len(matching_keys) > 2:
+                        self.ids['output2'].text = matching_keys[2]
+                    else:
+                        self.ids['output2'].text = ''
+                    if len(matching_keys) > 3:
+                        self.ids['output3'].text = matching_keys[3]
+                    else:
+                        self.ids['output3'].text = ''
+
                     out += matching_keys[0] + ' '
                 else:
                     out += word + ' '
 
-            self.ids.output.text = out
+            self.ids.input.text = out
 
 
 
@@ -127,6 +214,7 @@ class SecondScreen(Screen):
             dictionary.append([key, value, 0])            
 
         with open('dictionary.json', 'w') as file:
+            print('saving', dictionary)
             file.write(json.dumps(dictionary))
 
         self.ids.input.text = ''
@@ -146,10 +234,10 @@ class Dict_elements(GridLayout):
         #self.orientation = 'vertical'
         #self.size_hint = (1, None)
         #self.height = self.minimum_height
-        load_dictionary()
         self.refresh()
 
     def refresh(self):
+        load_dictionary()
         self.clear_widgets()
 
         for i, key in enumerate(keys):
